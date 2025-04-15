e0f0ef88d43d99e7efa93214d4a14bf8b8b4b181
Wes Lowe <wesley.lowe@yahoo.com>
2025-04-14 21:10:17 -0500

    refactor: enhance button active state; update chat sidebar layout and styles; improve default theme styles

--- src/routes/(chat)/-components/chat-sidebar.tsx
+++ src/routes/(chat)/-components/chat-sidebar.tsx
@@ -1,7 +1,7 @@
 import { Link } from "@tanstack/react-router";
-import "./chat-sidebar.scss";
-import { PlusIcon } from "lucide-react";
+import { MessageCirclePlusIcon, SearchIcon } from "lucide-react";
 import { Button, buttonVariants } from "../../../components/button";
+import "./chat-sidebar.scss";
 
 interface ChatLinkProps {
 	id: string;
@@ -19,15 +19,28 @@ const ChatLink = ({ id, title }: ChatLinkProps) => {
 export const ChatSidebar = () => {
 	return (
 		<div className="chat-sidebar">
-			<Link
-				to="/"
-				className={buttonVariants({ className: "new-chat-button", color: "secondary" })}
-			>
-				New chat
-			</Link>
-			<div className="chat-links">
-				<ChatLink id="1" title="Chat 1" />
-				<ChatLink id="2" title="Chat 2" />
+			<div className="chat-sidebar-actions">
+				<Button size="icon" variant="ghost" color="secondary">
+					<SearchIcon />
+				</Button>
+				<Link
+					to="/"
+					className={buttonVariants({
+						size: "icon",
+						color: "secondary",
+						variant: "ghost",
+					})}
+				>
+					<MessageCirclePlusIcon />
+				</Link>
+			</div>
+			<div className="chat-sidebar-main">
+				<div className="chat-links">
+					<ChatLink id="1" title="Chat 1" />
+					<ChatLink id="2" title="Chat 2" />
+					<ChatLink id="3" title="Chat 3" />
+					<ChatLink id="4" title="Chat 4" />
+				</div>
 			</div>
 		</div>
 	);
