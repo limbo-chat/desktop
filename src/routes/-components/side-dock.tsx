fda6bcfa695934cf7ad1f8519c20a4331ff80b08
Wes Lowe <wesley.lowe@yahoo.com>
2025-04-28 15:39:07 -0500

    added tooltips to side dock

--- src/routes/-components/side-dock.tsx
+++ src/routes/-components/side-dock.tsx
@@ -1,15 +1,20 @@
 import { Link } from "@tanstack/react-router";
 import { MessageSquareIcon, SettingsIcon } from "lucide-react";
+import { Tooltip } from "../../components/tooltip";
 
 export const SideDock = () => {
 	return (
 		<div className="side-dock">
-			<Link to="/chat" className="side-dock-item">
-				<MessageSquareIcon size={20} />
-			</Link>
-			<Link to="/settings" className="side-dock-item">
-				<SettingsIcon size={20} />
-			</Link>
+			<Tooltip label="Chat" positioning={{ placement: "right" }}>
+				<Link to="/chat" className="side-dock-item">
+					<MessageSquareIcon size={20} />
+				</Link>
+			</Tooltip>
+			<Tooltip label="Settings" positioning={{ placement: "right" }}>
+				<Link to="/settings" className="side-dock-item">
+					<SettingsIcon size={20} />
+				</Link>
+			</Tooltip>
 		</div>
 	);
 };
