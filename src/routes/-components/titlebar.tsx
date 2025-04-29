b447e6923a7d8482b22f3a1a68ee83397ffbf3a4
Wes Lowe <wesley.lowe@yahoo.com>
2025-04-29 08:59:33 -0500

    removed the titlebar for watching if the app is focused in favor of an app-focused classname applied to the root

--- src/routes/-components/titlebar.tsx
+++ src/routes/-components/titlebar.tsx
@@ -1,8 +1,3 @@
-import clsx from "clsx";
-import { useIsAppFocused } from "../../hooks/common";
-
 export const Titlebar = () => {
-	const isAppFocused = useIsAppFocused();
-
-	return <div className={clsx("titlebar", isAppFocused && "app-focused")}></div>;
+	return <div className="titlebar"></div>;
 };
