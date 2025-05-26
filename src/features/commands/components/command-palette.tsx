90522532551f3e14dfecc6af5920642f7d702d85
Wes Lowe <wesley.lowe@yahoo.com>
2025-05-25 22:40:49 -0500

    added fuzzy search

--- src/features/commands/components/command-palette.tsx
+++ src/features/commands/components/command-palette.tsx
@@ -1,11 +1,30 @@
+import Fuse from "fuse.js";
+import { useMemo, useState } from "react";
 import { DialogContent, DialogRoot } from "../../../components/dialog";
 import { useCommandList, useCommands, useIsCommandPaletteOpen } from "../hooks";
 import { setIsCommandPaletteOpen } from "../utils";
 
 export const CommandPalette = () => {
+	const commands = useCommandList();
 	const commandMap = useCommands();
-	const commandList = useCommandList();
 	const isCommandPaletteOpen = useIsCommandPaletteOpen();
+	const [search, setSearch] = useState("");
+
+	const fuse = useMemo(() => {
+		return new Fuse(commands, {
+			threshold: 0.3,
+			ignoreLocation: true,
+			keys: ["id", "name"],
+		});
+	}, [commands]);
+
+	const filteredCommands = useMemo(() => {
+		if (!search) {
+			return commands;
+		}
+
+		return fuse.search(search).map((item) => item.item);
+	}, [fuse, search, commands]);
 
 	const executeCommand = async (commandId: string) => {
 		const command = commandMap.get(commandId);
@@ -13,6 +32,11 @@ export const CommandPalette = () => {
 		if (!command) {
 			return;
 		}
+
+		// should the command palette be closed instantly or after the command is executed?
+
+		setIsCommandPaletteOpen(false);
+		setSearch("");
 
 		try {
 			await command.execute();
@@ -33,11 +57,18 @@ export const CommandPalette = () => {
 			open={isCommandPaletteOpen}
 			onOpenChange={({ open }) => {
 				setIsCommandPaletteOpen(open);
+				setSearch("");
 			}}
 		>
 			<DialogContent>
+				<input
+					placeholder="Search commands"
+					value={search}
+					onChange={(e) => setSearch(e.target.value)}
+				/>
 				<div>
-					{commandList.map((command) => (
+					{filteredCommands.length === 0 && <p>No commands found</p>}
+					{filteredCommands.map((command) => (
 						<button key={command.id} onClick={() => executeCommand(command.id)}>
 							{command.name}
 						</button>
