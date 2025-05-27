e7b5ed63e69f63b487e0a4532def38d16fdd2c77
Wes Lowe <wesley.lowe@yahoo.com>
2025-05-27 10:12:30 -0500

    added command palette action to side dock

--- src/routes/-components/side-dock.tsx
+++ src/routes/-components/side-dock.tsx
@@ -1,19 +1,25 @@
 import { Link } from "@tanstack/react-router";
-import { MessageSquareIcon, SettingsIcon } from "lucide-react";
+import { MessageSquareIcon, SettingsIcon, TerminalIcon } from "lucide-react";
 import { Tooltip } from "../../components/tooltip";
+import { setIsCommandPaletteOpen } from "../../features/commands/utils";
 
 export const SideDock = () => {
 	return (
 		<div className="side-dock">
 			<Tooltip label="Chat" positioning={{ placement: "right" }}>
 				<Link to="/chat" className="side-dock-item">
-					<MessageSquareIcon size={20} />
+					<MessageSquareIcon />
 				</Link>
 			</Tooltip>
 			<Tooltip label="Settings" positioning={{ placement: "right" }}>
 				<Link to="/settings" className="side-dock-item">
-					<SettingsIcon size={20} />
+					<SettingsIcon />
 				</Link>
+			</Tooltip>
+			<Tooltip label="Open command palette" positioning={{ placement: "right" }}>
+				<button className="side-dock-item" onClick={() => setIsCommandPaletteOpen(true)}>
+					<TerminalIcon />
+				</button>
 			</Tooltip>
 		</div>
 	);
