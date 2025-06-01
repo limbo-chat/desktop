63f37a03bbaebc01c02e0f19698dacc28c1b2584
Wes Lowe <wesley.lowe@yahoo.com>
2025-05-31 22:21:17 -0500

    refactor: enhance Checkbox component to accept custom className

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,11 +1,12 @@
 import * as RadixCheckbox from "@radix-ui/react-checkbox";
+import clsx from "clsx";
 import { CheckIcon } from "lucide-react";
 
 export interface CheckboxProps extends RadixCheckbox.CheckboxProps {}
 
-export const Checkbox = (props: CheckboxProps) => {
+export const Checkbox = ({ className, ...props }: CheckboxProps) => {
 	return (
-		<RadixCheckbox.Root className="checkbox">
+		<RadixCheckbox.Root className={clsx("checkbox")} {...props}>
 			<RadixCheckbox.Indicator className="checkbox-indicator">
 				<CheckIcon />
 			</RadixCheckbox.Indicator>
