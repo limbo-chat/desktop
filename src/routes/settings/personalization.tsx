9485de147c03e2e78933d40be966e3fbeaab04e6
Wes Lowe <wesley.lowe@yahoo.com>
2025-06-18 23:05:10 -0500

    updated personalization form to use the form controller

--- src/routes/settings/personalization.tsx
+++ src/routes/settings/personalization.tsx
@@ -1,9 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { Anchor } from "../../components/anchor";
 import { Button } from "../../components/button";
-import { Field } from "../../components/field";
-import * as FieldPrimitive from "../../components/field-primitive";
+import * as FieldController from "../../components/field-controller";
 import {
 	Form,
 	FormActions,
@@ -12,15 +11,6 @@ import {
 	FormSection,
 	FormSectionContent,
 } from "../../components/form";
-import {
-	SettingItem,
-	SettingItemControl,
-	SettingItemDescription,
-	SettingItemInfo,
-	SettingItemTitle,
-	SettingsSection,
-	SettingsSectionContent,
-} from "../../components/settings";
 import {
 	useGetSettingsSuspenseQuery,
 	useUpdateSettingsMutation,
@@ -50,63 +40,64 @@ function PersonalizationSettingsPage() {
 	return (
 		<SettingsPage data-page="personalization">
 			<SettingsPageContent>
-				<Form onSubmit={handleSubmit}>
-					<FormContent>
-						<FormSection>
-							<FormSectionContent>
-								<Field
-									id="username"
-									label="Username"
-									description="What would you like to be called?"
+				<FormProvider {...form}>
+					<Form onSubmit={handleSubmit}>
+						<FormContent>
+							<FormSection>
+								<FormSectionContent>
+									<FieldController.Root
+										id="username"
+										name="username"
+										label="Username"
+										description="What would you like to be called?"
+									>
+										<FieldController.TextInput placeholder="Enter your name" />
+									</FieldController.Root>
+									<FieldController.Root
+										id="system-prompt"
+										name="systemPrompt"
+										label="System prompt"
+										description={
+											<>
+												This prompt is used to set the context for all chat
+												completions. Learn about{" "}
+												<Anchor
+													href="https://handlebarsjs.com"
+													target="_blank"
+													tabIndex={-1}
+												>
+													Handlebars
+												</Anchor>
+												.
+											</>
+										}
+									>
+										<FieldController.Textarea />
+									</FieldController.Root>
+								</FormSectionContent>
+							</FormSection>
+						</FormContent>
+						<FormFooter>
+							<FormActions>
+								<Button
+									type="button"
+									data-action="cancel"
+									disabled={!form.formState.isDirty}
+									onClick={() => form.reset()}
 								>
-									<FieldPrimitive.TextInput
-										placeholder="Enter your name"
-										{...form.register("username")}
-									/>
-								</Field>
-								<Field
-									id="system-prompt"
-									label="System prompt"
-									description={
-										<>
-											This prompt is used to set the context for all chat
-											completions. Learn about{" "}
-											<Anchor
-												href="https://handlebarsjs.com"
-												target="_blank"
-												tabIndex={-1}
-											>
-												Handlebars
-											</Anchor>
-											.
-										</>
-									}
+									Cancel
+								</Button>
+								<Button
+									type="submit"
+									data-action="submit"
+									disabled={!form.formState.isDirty}
 								>
-									<FieldPrimitive.Textarea {...form.register("systemPrompt")} />
-								</Field>
-							</FormSectionContent>
-						</FormSection>
-					</FormContent>
-					<FormFooter>
-						<FormActions>
-							<Button
-								type="button"
-								data-action="cancel"
-								disabled={!form.formState.isDirty}
-								onClick={() => form.reset()}
-							>
-								Cancel
-							</Button>
-							<Button
-								type="submit"
-								data-action="submit"
-								disabled={!form.formState.isDirty}
-							>
-								Save changes
-							</Button>
-						</FormActions>
-					</FormFooter>
-				</Form>
+									Save changes
+								</Button>
+							</FormActions>
+						</FormFooter>
+					</Form>
+				</FormProvider>
 			</SettingsPageContent>
 		</SettingsPage>
 	);
