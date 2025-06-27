e021e40b9f0447b695b7e3fb4ecc42f1c71a3c51
Wes Lowe <wesley.lowe@yahoo.com>
2025-06-26 22:56:12 -0500

    add onboarding completion logic to update metadata

--- src/main/trpc/router/onboarding.ts
+++ src/main/trpc/router/onboarding.ts
@@ -1,7 +1,15 @@
+import { readMeta, writeMeta } from "../../meta/utils";
 import { publicProcedure, router } from "../trpc";
 
 export const onboardingRouter = router({
 	complete: publicProcedure.mutation(({ ctx }) => {
+		const oldMeta = readMeta();
+
+		writeMeta({
+			...oldMeta,
+			completedOnboarding: true,
+		});
+
 		const onboardingWindow = ctx.windowManager.getWindow("onboarding");
 
 		// this should always be a BrowserWindow, but just in case
