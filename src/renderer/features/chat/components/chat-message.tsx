77b34d7d915a6dae943ec44a51b08ced4c5bea55
Wes Lowe <wesley.lowe@yahoo.com>
2025-07-13 14:56:34 -0500

    added structure for chat message content and footer

--- src/renderer/features/chat/components/chat-message.tsx
+++ src/renderer/features/chat/components/chat-message.tsx
@@ -1,4 +1,7 @@
 import { memo, type HTMLAttributes } from "react";
+import { AppIcon } from "../../../components/app-icon";
+import { IconButton } from "../../../components/icon-button";
+import { Tooltip } from "../../../components/tooltip";
 import { ChatNodeRenderer } from "../../chat-nodes/components/chat-node-renderer";
 import type {
 	AssistantChatMessage as AssistantChatMessageRenderer,
@@ -14,6 +17,18 @@ const ChatMessageContainer = ({ message, ...props }: ChatMessageContainerProps) 
 	return <div className="chat-message" data-role={message.role} {...props} />;
 };
 
+const ChatMessageContent = (props: HTMLAttributes<HTMLDivElement>) => {
+	return <div className="chat-message-content" {...props} />;
+};
+
+const ChatMessageFooter = (props: HTMLAttributes<HTMLDivElement>) => {
+	return <div className="chat-message-footer" {...props} />;
+};
+
+const ChatMessageActions = (props: HTMLAttributes<HTMLDivElement>) => {
+	return <div className="chat-message-actions" {...props} />;
+};
+
 interface UserChatMessageRendererProps {
 	message: UserChatMessageRenderer;
 }
@@ -21,9 +36,20 @@ interface UserChatMessageRendererProps {
 const UserChatMessageRenderer = ({ message }: UserChatMessageRendererProps) => {
 	return (
 		<ChatMessageContainer message={message}>
-			{message.content.map((node, idx) => (
-				<ChatNodeRenderer node={node} key={idx} />
-			))}
+			<ChatMessageContent>
+				{message.content.map((node, idx) => (
+					<ChatNodeRenderer node={node} key={idx} />
+				))}
+			</ChatMessageContent>
+			<ChatMessageFooter>
+				<ChatMessageActions>
+					<Tooltip label="Copy message">
+						<IconButton action="copy-message">
+							<AppIcon icon="copy" />
+						</IconButton>
+					</Tooltip>
+				</ChatMessageActions>
+			</ChatMessageFooter>
 		</ChatMessageContainer>
 	);
 };
@@ -35,9 +61,20 @@ interface AssistantChatMessageRendererProps {
 const AssistantChatMessageRenderer = ({ message }: AssistantChatMessageRendererProps) => {
 	return (
 		<ChatMessageContainer message={message} data-status={message.status}>
-			{message.content.map((node, idx) => (
-				<ChatNodeRenderer node={node} key={idx} />
-			))}
+			<ChatMessageContent>
+				{message.content.map((node, idx) => (
+					<ChatNodeRenderer node={node} key={idx} />
+				))}
+			</ChatMessageContent>
+			<ChatMessageFooter>
+				<ChatMessageActions>
+					<Tooltip label="Copy message">
+						<IconButton action="copy-message">
+							<AppIcon icon="copy" />
+						</IconButton>
+					</Tooltip>
+				</ChatMessageActions>
+			</ChatMessageFooter>
 		</ChatMessageContainer>
 	);
 };
