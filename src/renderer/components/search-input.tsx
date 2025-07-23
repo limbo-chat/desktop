7f75e022b52d395b203a4b59a2109480c7d8ff6e
Wes Lowe <wesley.lowe@yahoo.com>
2025-07-22 21:59:10 -0500

    add search input component and add filtering to view-assistants

---
+++ src/renderer/components/search-input.tsx
@@ -0,0 +1,33 @@
+import clsx from "clsx";
+import { AppIcon } from "./app-icon";
+import { IconButton } from "./icon-button";
+
+export interface SearchInputProps
+	extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "value" | "onChange"> {
+	ref?: React.RefObject<HTMLInputElement | null>;
+	value: string;
+	onChange: (value: string) => void;
+}
+
+export const SearchInput = ({ value, onChange, className, ref, ...props }: SearchInputProps) => {
+	return (
+		<div className={clsx("search-input-container", className)}>
+			<div className="search-input-icon">
+				<AppIcon icon="search" />
+			</div>
+			<input
+				type="search"
+				className="search-input"
+				value={value}
+				onChange={(e) => onChange(e.target.value)}
+				ref={ref}
+				{...props}
+			/>
+			{value.length > 0 && (
+				<IconButton className="search-input-clear-button" onClick={() => onChange("")}>
+					<AppIcon icon="close" />
+				</IconButton>
+			)}
+		</div>
+	);
+};
--- src/renderer/features/assistants/components/views/view-assistants.tsx
+++ src/renderer/features/assistants/components/views/view-assistants.tsx
@@ -1,8 +1,11 @@
 import { useSuspenseQuery } from "@tanstack/react-query";
+import Fuse from "fuse.js";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { AppIcon } from "../../../../components/app-icon";
 import { Button } from "../../../../components/button";
 import { EmptyState, EmptyStateActions, EmptyStateTitle } from "../../../../components/empty-state";
 import { IconButton } from "../../../../components/icon-button";
+import { SearchInput } from "../../../../components/search-input";
 import { Tooltip } from "../../../../components/tooltip";
 import { useMainRouter } from "../../../../lib/trpc";
 import * as View from "../../../view-stack/components/view";
@@ -13,11 +16,44 @@ export const AssistantsView = () => {
 	const getAssistantsQuery = useSuspenseQuery(mainRouter.assistants.getAll.queryOptions());
 	const assistants = getAssistantsQuery.data;
 	const viewStack = useViewStackContext();
+	const [search, setSearch] = useState("");
+	const searchInputRef = useRef<HTMLInputElement>(null);
+
+	const fuse = useMemo(() => {
+		return new Fuse(assistants, {
+			threshold: 0.3,
+			ignoreLocation: true,
+			keys: ["id", "name", "description"],
+		});
+	}, [assistants]);
+
+	const filteredAssistants = useMemo(() => {
+		if (search.length === 0) {
+			return assistants;
+		}
+
+		return fuse.search(search).map((item) => item.item);
+	}, [fuse, search, assistants]);
+
+	useEffect(() => {
+		// this seems to fix the issue of the input not focusing on mount
+		// not sure why I have to do this, may be due to being rendered inside radix tabs
+		requestAnimationFrame(() => {
+			if (searchInputRef.current) {
+				searchInputRef.current.focus();
+			}
+		});
+	}, []);
 
 	return (
 		<View.Root>
 			<View.Header>
-				<input type="text" className="assistants-view-search-input" />
+				<SearchInput
+					placeholder="Search assistants..."
+					value={search}
+					onChange={setSearch}
+					ref={searchInputRef}
+				/>
 				<View.HeaderActions>
 					<Tooltip label="Download assistant">
 						<IconButton onClick={() => {}}>
@@ -48,21 +84,23 @@ export const AssistantsView = () => {
 						</EmptyStateActions>
 					</EmptyState>
 				)}
-				{assistants.map((assistant) => (
-					<div
-						className="assistant"
-						key={assistant.id}
-						onClick={() =>
-							viewStack.push({
-								id: "update-assistant",
-								data: { assistantId: assistant.id },
-							})
-						}
-					>
-						<div className="assistant-name">{assistant.name}</div>
-						<p className="assistant-description">{assistant.description}</p>
-					</div>
-				))}
+				<div className="assistant-list">
+					{filteredAssistants.map((assistant) => (
+						<div
+							className="assistant"
+							key={assistant.id}
+							onClick={() =>
+								viewStack.push({
+									id: "update-assistant",
+									data: { assistantId: assistant.id },
+								})
+							}
+						>
+							<div className="assistant-name">{assistant.name}</div>
+							<p className="assistant-description">{assistant.description}</p>
+						</div>
+					))}
+				</div>
 			</View.Content>
 		</View.Root>
 	);
