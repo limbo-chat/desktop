42e95f901df126006ae197c4ab4b5d4e3b9229c4
Wes Lowe <wesley.lowe@yahoo.com>
2025-08-07 07:13:01 -0500

    removed unused oauth function and schema

--- src/main/auth/helpers/oauth.ts
+++ src/main/auth/helpers/oauth.ts
@@ -1,9 +1,5 @@
 import { AUTH_REDIRECT_URI } from "../constants";
-import {
-	dynamicClientRegistrationResponseSchema,
-	tokenResponseSchema,
-	wellKnownOAuthConfigSchema,
-} from "../schemas";
+import { dynamicClientRegistrationResponseSchema, tokenResponseSchema } from "../schemas";
 
 // utility functions
 
@@ -38,29 +34,6 @@ export function buildOAuthAuthorizationUrl(opts: BuildOAuthAuthorizationUrlOptio
 	}
 
 	return authUrl;
-}
-
-export async function readWellKnownConfiguration(issuerUrl: string) {
-	const wellKnownUrl = new URL(".well-known/openid-configuration", issuerUrl);
-
-	const response = await fetch(wellKnownUrl.toString(), {
-		headers: {
-			Accept: "application/json",
-		},
-	});
-
-	if (!response.ok) {
-		throw new Error(`Failed to fetch well-known configuration from ${wellKnownUrl.toString()}`);
-	}
-
-	const body = await response.json();
-	const responseParseResult = wellKnownOAuthConfigSchema.safeParse(body);
-
-	if (!responseParseResult.success) {
-		throw new Error("Received invalid well-known OAuth server configuration");
-	}
-
-	return responseParseResult.data;
 }
 
 // oauth operations
