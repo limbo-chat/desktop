a9047324ce5cb453d7fe66b0adbc4cb89c139b7a
Wes Lowe <wesley.jlowe10@gmail.com>
2025-08-16 19:46:53 -0500

    converted the command palette to use a list quick picker, deleted the search prompt component

--- src/renderer/features/commands/components/command-palette.tsx
+++ src/renderer/features/commands/components/command-palette.tsx
@@ -1,21 +1,10 @@
 import { useMemo, useState } from "react";
 import Fuse from "fuse.js";
-import {
-	EmptyResult,
-	SearchPrompt,
-	SearchPromptInput,
-	SearchPromptInputContainer,
-	SearchPromptInstructionCommand,
-	SearchPromptInstructionItem,
-	SearchPromptInstructions,
-	SearchPromptInstructionText,
-	SearchPromptResults,
-	ResultContent,
-	ResultItem,
-	ResultTitle,
-} from "../../../components/search-prompt";
+import { EmptyStateDescription, EmptyStateTitle } from "../../../components/empty-state";
 import { useModalContext } from "../../modals/hooks";
 import { showNotification } from "../../notifications/utils";
+import * as ListQuickPicker from "../../quick-picker/components/list-primitive";
+import * as QuickPicker from "../../quick-picker/components/primitive";
 import { useCommandList, useCommands } from "../hooks";
 
 export interface CommandPaletteProps {
@@ -23,6 +12,7 @@ export interface CommandPaletteProps {
 }
 
 export const CommandPalette = ({ onExecuteCommand }: CommandPaletteProps) => {
+	const [focusedCommandId, setFocuseCommandId] = useState<string | null>(null);
 	const [search, setSearch] = useState("");
 
 	const commands = useCommandList();
@@ -43,40 +33,57 @@ export const CommandPalette = ({ onExecuteCommand }: CommandPaletteProps) => {
 		return fuse.search(search).map((item) => item.item);
 	}, [fuse, search, commands]);
 
+	const filteredCommandIds = useMemo(() => {
+		return filteredCommands.map((item) => item.id);
+	}, [filteredCommands]);
+
+	const handleSelect = (commandId: string) => {
+		onExecuteCommand(commandId);
+	};
+
 	return (
-		<SearchPrompt className="command-palette" label="Command palette" loop shouldFilter={false}>
-			<SearchPromptInputContainer>
-				<SearchPromptInput
-					placeholder="Search commands..."
-					value={search}
-					onValueChange={setSearch}
-				/>
-			</SearchPromptInputContainer>
-			<SearchPromptResults>
-				{filteredCommands.length === 0 && <EmptyResult>No commands found</EmptyResult>}
-				{filteredCommands.map((command) => (
-					<ResultItem key={command.id} onSelect={() => onExecuteCommand(command.id)}>
-						<ResultContent>
-							<ResultTitle>{command.name}</ResultTitle>
-						</ResultContent>
-					</ResultItem>
-				))}
-			</SearchPromptResults>
-			<SearchPromptInstructions>
-				<SearchPromptInstructionItem>
-					<SearchPromptInstructionCommand>↑↓</SearchPromptInstructionCommand>
-					<SearchPromptInstructionText>to navigate</SearchPromptInstructionText>
-				</SearchPromptInstructionItem>
-				<SearchPromptInstructionItem>
-					<SearchPromptInstructionCommand>↵</SearchPromptInstructionCommand>
-					<SearchPromptInstructionText>to use</SearchPromptInstructionText>
-				</SearchPromptInstructionItem>
-				<SearchPromptInstructionItem>
-					<SearchPromptInstructionCommand>esc</SearchPromptInstructionCommand>
-					<SearchPromptInstructionText>to dismiss</SearchPromptInstructionText>
-				</SearchPromptInstructionItem>
-			</SearchPromptInstructions>
-		</SearchPrompt>
+		<ListQuickPicker.Root
+			className="command-palette"
+			items={filteredCommandIds}
+			focusedId={focusedCommandId}
+			selectedId={null}
+			onFocusedIdChange={setFocuseCommandId}
+			onSelectedIdChange={handleSelect}
+		>
+			<QuickPicker.Header>
+				<QuickPicker.Search>
+					<QuickPicker.SearchInput
+						placeholder="Search commands..."
+						value={search}
+						onChange={(e) => {
+							setSearch(e.target.value);
+						}}
+					/>
+				</QuickPicker.Search>
+			</QuickPicker.Header>
+			<QuickPicker.Content>
+				{filteredCommands.length > 0 ? (
+					<ListQuickPicker.List>
+						{filteredCommands.map((item) => (
+							<ListQuickPicker.ListItem
+								item={{
+									id: item.id,
+									title: item.name,
+									description: "",
+									icon: null,
+								}}
+								key={item.id}
+							/>
+						))}
+					</ListQuickPicker.List>
+				) : (
+					<QuickPicker.EmptyState>
+						<EmptyStateTitle>No commands found</EmptyStateTitle>
+						<EmptyStateDescription>Try another search</EmptyStateDescription>
+					</QuickPicker.EmptyState>
+				)}
+			</QuickPicker.Content>
+		</ListQuickPicker.Root>
 	);
 };
 
